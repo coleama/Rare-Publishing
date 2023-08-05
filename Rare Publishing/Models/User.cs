a2cadedd1b8ee3e694584996e8fc6a08e6a1461a
coleama <cole.ama@gmail.com>
2023-08-05 09:59:17 -0500

    added user

--- Rare Publishing/Models/User.cs
+++ Rare Publishing/Models/User.cs
@@ -5,6 +5,7 @@ namespace Rare_Publishing.Modules
     public class User
     {
        public int Id { get; set; }
+        public string? first_name { get; set; }
         public string? last_name { get; set;}
         public string? email { get; set; }
         public string? password { get; set; }
