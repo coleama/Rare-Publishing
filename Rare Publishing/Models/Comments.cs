c2bd4ba9ac2e78dd4823dc80a094d643fe982904
Demansell <demansell2016@gmail.com>
2023-08-05 10:16:13 -0500

    Added comments and tags classes in models folder

---
+++ Rare Publishing/Models/Comments.cs
@@ -0,0 +1,11 @@
+﻿namespace Rare_Publishing.Models
+{
+    public class Comments
+    {
+        public int Id { get; set; }
+        public int Author_id { get; set; }
+        public int Post_id { get; set; }
+        public string content { get; set; }
+
+    }
+}
---
+++ Rare Publishing/Models/Tags.cs
@@ -0,0 +1,8 @@
+﻿namespace Rare_Publishing.Models
+{
+    public class tags
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+    }
+}
