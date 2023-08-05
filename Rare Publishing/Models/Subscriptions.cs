3809278082b23bd8a8df6bad93c52c1787565821
rtullis98 <121829715+rtullis98@users.noreply.github.com>
2023-08-05 10:07:32 -0500

    Pushing up subscriptions and categories

---
+++ Rare Publishing/Models/Subscriptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Rare_Publishing.Models
+{
+    public class Subscriptions
+    {
+        public int Id { get; set; }
+        public int Follower_id { get; set; }
+        public int Author_id { get; set; }
+        public DateTime? CreatedOn { get; set; }
+
+    }
+}
